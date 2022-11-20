361c3d5e765071c04bca40f3003469ac4a0a67be
Tiago Rodrigues Cunha <tiago.r.c.sn@hotmail.com>
2022-11-20 19:14:16 -0300

    add json albuns and some cards

--- src/createCollectionsAndData.py
+++ src/createCollectionsAndData.py
@@ -1,9 +1,8 @@
 import logging
 from conexion.mongo_queries import MongoQueries
-from conexion.oracle_queries import OracleQueries
 import json
 
-LIST_OF_COLLECTIONS = ["fornecedores", "clientes", "produtos", "pedidos", "itens_pedido"]
+LIST_OF_COLLECTIONS = ["album", "user", "card"]
 logger = logging.getLogger(name="Example_CRUD_MongoDB")
 logger.setLevel(level=logging.WARNING)
 mongo = MongoQueries()
@@ -37,22 +36,10 @@ def insert_many(data:json, collection:str):
     mongo.db[collection].insert_many(data)
     mongo.close()
 
-def extract_and_insert():
-    oracle = OracleQueries()
-    oracle.connect()
-    sql = "select * from labdatabase.{table}"
-    for collection in LIST_OF_COLLECTIONS:
-        df = oracle.sqlToDataFrame(sql.format(table=collection))
-        if collection == "pedidos":
-            df["data_pedido"] = df["data_pedido"].dt.strftime("%m-%d-%Y")
-        logger.warning(f"data extracted from database Oracle labdatabase.{collection}")
-        records = json.loads(df.T.to_json()).values()
-        logger.warning("data converted to json")
-        insert_many(data=records, collection=collection)
-        logger.warning(f"documents generated at {collection} collection")
-
 if __name__ == "__main__":
     logging.warning("Starting")
     createCollections(drop_if_exists=True)
-    extract_and_insert()
+    with open("albums.json", "r") as f:
+            albums = json.load(f)
+    insert_many(albums, "album")
     logging.warning("End")
